ecf3c607a223a771b3f2d26b6209cfffa449098c
Caroline <carolinefernandesc@hotmail.com>
2023-02-27 17:23:07 -0300

    requisito 04

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from './services/renderWithRouter';
+import { FavoritePokemon } from '../pages';
+// import App from '../App';
+
+describe('Requisito 03', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
+    renderWithRouter(<FavoritePokemon />);
+
+    const noFavorite = screen.getByText(/No favorite Pokémon found/i);
+
+    expect(noFavorite).toBeVisible();
+  });
+
+  // test('Teste se apenas são exibidos os Pokémon favoritados', () => {
+  //   const { history } = renderWithRouter(<App />);
+
+  //   const linkMoreDetails = screen.getByRole('link', { name: /More details/i });
+  //   userEvent.click(linkMoreDetails);
+
+  //   const pokemonFavorite = screen.getByLabelText(/Pokémon favoritado/i);
+  //   userEvent.click(pokemonFavorite);
+  //   history.push('/favorites');
+
+  //   const namePokemon = screen.getByTestId('pokemon-name');
+  //   expect(namePokemon).toBeVisible();
+  // });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './services/renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Requisito 04', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const title = screen.getByRole('heading', { name: /Page requested not found/i });
+
+    expect(title).toBeVisible();
+  });
+
+  test('Teste se a página mostra a imagem => (https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif)', () => {
+    renderWithRouter(<NotFound />);
+
+    const img = screen.getByAltText(/Pikachu crying because the page requested was not found/i);
+
+    expect(img.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
