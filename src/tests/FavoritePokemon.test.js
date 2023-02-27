2f40bd6a0b65cba2570a670a747fd1b18277b7a2
Caroline <carolinefernandesc@hotmail.com>
2023-02-27 17:56:35 -0300

    finalizando e corrigindo requisito 03

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,8 +1,9 @@
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 import renderWithRouter from './services/renderWithRouter';
 import { FavoritePokemon } from '../pages';
-// import App from '../App';
+import App from '../App';
 
 describe('Requisito 03', () => {
   test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos', () => {
@@ -13,17 +14,23 @@ describe('Requisito 03', () => {
     expect(noFavorite).toBeVisible();
   });
 
-  // test('Teste se apenas são exibidos os Pokémon favoritados', () => {
-  //   const { history } = renderWithRouter(<App />);
+  test('Teste se apenas são exibidos os Pokémon favoritados', () => {
+    const { history } = renderWithRouter(<App />);
 
-  //   const linkMoreDetails = screen.getByRole('link', { name: /More details/i });
-  //   userEvent.click(linkMoreDetails);
+    const linkMoreDetails = screen.getByRole('link', { name: /More details/i });
+    userEvent.click(linkMoreDetails);
+    act(() => {
+      history.push('/pokemon/4');
+    });
 
-  //   const pokemonFavorite = screen.getByLabelText(/Pokémon favoritado/i);
-  //   userEvent.click(pokemonFavorite);
-  //   history.push('/favorites');
+    const pokemonFavorite = screen.getByLabelText(/Pokémon favoritado/i);
+    expect(pokemonFavorite).toBeVisible();
+    userEvent.click(pokemonFavorite);
+    act(() => {
+      history.push('/favorites');
+    });
 
-  //   const namePokemon = screen.getByTestId('pokemon-name');
-  //   expect(namePokemon).toBeVisible();
-  // });
+    const namePokemonFavorite = screen.getByText(/Charmander/i);
+    expect(namePokemonFavorite).toBeVisible();
+  });
 });
