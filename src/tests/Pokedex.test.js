9a2d43508847198b89c6074e8c6dfac803f8911a
Caroline <carolinefernandesc@hotmail.com>
2023-02-28 12:30:24 -0300

    corrigindo erros de digitação

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getAllByRole, getByRole, getByText, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './services/renderWithRouter';
 import App from '../App';
