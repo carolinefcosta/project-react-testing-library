b1a9b293ff6b3d025fae09633aa5d1e4dc98db0b
Caroline <carolinefernandesc@hotmail.com>
2023-02-28 15:38:10 -0300

    requisito 05

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -5,9 +5,6 @@ import renderWithRouter from './services/renderWithRouter';
 import App from '../App';
 
 describe('Requisito 05', () => {
-  // const pokemonList = [
-  //   'Pikachu', 'Charmander', 'Caterpie', 'Ekans', 'Alakazam', 'Mew', 'Rapidash', 'Snorlax', ''];
-
   test('Teste se a página contém um heading h2 com o texto Encountered Pokémon', () => {
     renderWithRouter(<App />);
 
@@ -82,24 +79,24 @@ describe('Requisito 05', () => {
     userEvent.click(buttonFire);
     const pokemonFire1 = screen.getByText(/Charmander/i);
     expect(pokemonFire1).toBeVisible();
-    expect(buttonAll).toBeVisible();
 
     const buttonNext = screen.getByTestId(/next-pokemon/i);
     userEvent.click(buttonNext);
     const pokemonFire2 = screen.getByText(/Rapidash/i);
     expect(pokemonFire2).toBeVisible();
-    expect(buttonAll).toBeVisible();
 
     const buttonPsychic = screen.getByRole('button', { name: /Psychic/i });
     userEvent.click(buttonPsychic);
     const pokemonPsychic1 = screen.getByText(/Alakazam/i);
     expect(pokemonPsychic1).toBeVisible();
-    expect(buttonAll).toBeVisible();
 
     userEvent.click(buttonNext);
     const pokemonPsychic2 = screen.getByText(/Mew/i);
     expect(pokemonPsychic2).toBeVisible();
-    expect(buttonAll).toBeVisible();
+
+    userEvent.click(buttonAll);
+    const initialPokemon = screen.getByText(/Pikachu/i);
+    expect(initialPokemon).toBeVisible();
   });
 
   test('Teste se a Pokédex contém um botão para resetar o filtro', () => {
@@ -107,47 +104,9 @@ describe('Requisito 05', () => {
 
     const buttonAll = screen.getByRole('button', { name: /All/i });
     expect(buttonAll).toBeVisible();
+
     userEvent.click(buttonAll);
     const pokemon1 = screen.getByText(/Pikachu/i);
     expect(pokemon1).toBeVisible();
-
-    const button = screen.getByRole('button', { name: /Próximo Pokémon/i });
-    expect(button).toBeVisible();
-
-    userEvent.click(button);
-    const pokemon2 = screen.getByText(/Charmander/i);
-    expect(pokemon2).toBeVisible();
-
-    userEvent.click(button);
-    const pokemon3 = screen.getByText(/Caterpie/i);
-    expect(pokemon3).toBeVisible();
-
-    userEvent.click(button);
-    const pokemon4 = screen.getByText(/Ekans/i);
-    expect(pokemon4).toBeVisible();
-
-    userEvent.click(button);
-    const pokemon5 = screen.getByText(/Alakazam/i);
-    expect(pokemon5).toBeVisible();
-
-    userEvent.click(button);
-    const pokemon6 = screen.getByText(/Mew/i);
-    expect(pokemon6).toBeVisible();
-
-    userEvent.click(button);
-    const pokemon7 = screen.getByText(/Rapidash/i);
-    expect(pokemon7).toBeVisible();
-
-    userEvent.click(button);
-    const pokemon8 = screen.getByText(/Snorlax/i);
-    expect(pokemon8).toBeVisible();
-
-    userEvent.click(button);
-    const pokemon9 = screen.getByText(/Dragonair/i);
-    expect(pokemon9).toBeVisible();
-
-    userEvent.click(button);
-    const pokemon10 = screen.getByText(/Pikachu/i);
-    expect(pokemon10).toBeVisible();
   });
 });
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import { act } from 'react-dom/test-utils';
+import renderWithRouter from './services/renderWithRouter';
+import App from '../App';
+
+describe('Requisito 06', () => {
+  test('Teste se é renderizado um card com as informações de determinado Pokémon', () => {
+    renderWithRouter(<App />);
+
+    const name = screen.getByTestId(/pokemon-name/i);
+    expect(name).toHaveTextContent(/Pikachu/i);
+
+    const type = screen.getAllByTestId(/pokemon-type/i);
+    expect(type[0]).toHaveTextContent(/Electric/i);
+
+    const weight = screen.getByTestId(/pokemon-weight/i);
+    expect(weight).toHaveTextContent(/Average weight: 6.0 kg/i);
+
+    const img = screen.getByAltText(/Pikachu sprite/i);
+    expect(img.src).toContain('https://archives.bulbagarden.net/media/upload/b/b2/Spr_5b_025_m.png');
+  });
+});
